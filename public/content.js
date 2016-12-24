22592f9d60eaf4a5821df2a7ed141a7347936062
doerschera <doerschera@gmail.com>
2016-12-24 13:05:01 -0600

    created resume content document

---
+++ public/content.js
@@ -0,0 +1,27 @@
+
+export default {
+  skills: ['HTML/CSS', 'SASS', 'Javascript', 'jQuery', 'Node', 'React', 'Express', 'Handlebars', 'Photoshop', 'Illustrator', 'InDesign'],
+  education: [
+    {
+      title: 'Undergraduate Degree',
+      school: 'Rhode Island School of Design',
+      program: 'Bachelor of Fine Arts',
+      area: 'Sculpture',
+      honors: true
+    },
+    {
+      title: 'Graduate Coursework',
+      school: 'University of Michigan',
+      program: 'Taubman College of Architecture & Urban Planning',
+      area: 'Master of Architecture Candidate',
+      honors: false
+    },
+    {
+      title: 'Technical Training',
+      school: 'University of Texas, Austin',
+      program: 'Center for Professional Education',
+      area: 'Fullstack Web Development Certificate',
+      honors: false
+    }
+  ]
+}
