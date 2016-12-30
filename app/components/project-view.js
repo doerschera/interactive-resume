3dc8110cfc3aa917b756ee6f472442cea063fef7
doerschera <doerschera@gmail.com>
2016-12-29 21:09:14 -0600

    move to next project

--- app/components/project-view.js
+++ app/components/project-view.js
@@ -1,13 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  currentProject: 0,
-
   didRender() {
     this._super(...arguments);
-    console.log('will render');
-    let currentProjectIndex = this.get('currentProject');
-    console.log(currentProjectIndex);
-    this.$('#project'+currentProjectIndex).css('display', 'flex');
+    this.$('#project0').css('display', 'flex');
   }
 });
--- app/controllers/work.js
+++ app/controllers/work.js
@@ -1,12 +1,19 @@
 import Ember from 'ember';
 import content from '../data/content';
-//
-// const $ = Ember.$;
+
+const $ = Ember.$;
 
 export default Ember.Controller.extend({
-  projects: content.projects,
   currentProject: 0,
-  actions: {
 
-  }
+  actions: {
+    nextProject() {
+      let activeProject = '#project'+this.currentProject;
+      this.currentProject ++
+      let nextProject = '#project'+this.currentProject;
+
+      $(activeProject).hide();
+      $(nextProject).css('display', 'flex');
+    }
+  },
 });
